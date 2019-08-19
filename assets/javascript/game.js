52fa1e970f551f4a97c5882346a7bbf92ea44560
Justin Carlson <justincarlson7@gmail.com>
2019-08-18 21:40:26 -0400

    working on javascript - defining variables and starting to think about the logic needed to play the game.

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+var wordOptions = ["apple", "peach", "pear", "orange", "fig"];
+
+var currentWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+
+console.log("This is the current word " + currentWord);
+
+var currentWordArray = []
+
+// document.onkeyup = function(event) {
+//   var playerGuess = event.key;
+
+//   console.log("this is the player guess " + playerGuess);
+// };
+
+;
+
+function letters(currentWordArray) {
+  currentWordArray = currentWord.split();
+
+  console.log("These are the letters " + currentWordArray);
+};
