2a54209bd973e5c70dde0c140db8ac35631e83f0
Justin Carlson <justincarlson7@gmail.com>
2019-08-23 18:43:44 -0400

    more work on the javascript file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,20 +2,48 @@ var wordOptions = ["apple", "peach", "pear", "orange", "fig"];
 
 var currentWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
 
+//create the correct number of underscores
+
 console.log("This is the current word " + currentWord);
 
 var currentWordArray = []
 
-// document.onkeyup = function(event) {
-//   var playerGuess = event.key;
+document.onkeyup = function(event) {
+var playerGuess = event.keyCode;
 
-//   console.log("this is the player guess " + playerGuess);
-// };
+// if keycode is between 65- 90 keep it, if not - do nothing or prompt to pick a different key 
+
+if(playerGuess >= 65 && playerGuess <= 90) {
+
+  var playerGuess = event.key;
+letters(currentWordArray, playerGuess);
+} else {
+
+  console.log("Pick another key!!!!")
+}
+
+
+
+};
 
 ;
 
-function letters(currentWordArray) {
-  currentWordArray = currentWord.split();
+function letters(currentWordArray, playerGuess) {
 
-  console.log("These are the letters " + currentWordArray);
+  currentWordArray = currentWord.split("");
+
+  for (var i = 0; i < currentWordArray.length; i++) {
+    
+//check to see if playerguess equals this letter and show it on the screen
+if(playerGuess === currentWordArray[i]) {
+}
+console.log(currentWordArray[i])
+
+    
+  //replace the underscore with the correct letter when player presses it
+
+  
+  console.log("These are the letters " + currentWordArray[0]);
+  console.log("Look here " + playerGuess);
 };
+}
